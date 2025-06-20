05cbf59b876885dd7c2abf21ba13ba09a289f7b6
Miwacer <il>
2025-06-20 23:09:02 +0300

    solution v2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import pprint
 
 load_dotenv()
 
@@ -15,9 +16,12 @@ def get_weather() -> None:
     url = "http://api.weatherapi.com/v1/current.json"
 
     response = requests.get(url, params=params)
-
+    country = response.json()["location"]["country"]
+    location = response.json()["location"]["name"]
     temp = response.json()["current"]["temp_c"]
-    print(temp)
+    time = response.json()["location"]["localtime"]
+
+    print(f'{country}/{location} - {time}, Weather: {temp} Celsius')
 
 if __name__ == "__main__":
     get_weather()
